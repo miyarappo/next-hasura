0e1da2c336a819904067bdd4f6c5aeaf04605337
Minoru Miyata <miyarappo@gmail.com>
2022-02-16 22:21:39 +0900

    Create hasura crud pages

---
+++ pages/hasura-main.tsx
@@ -0,0 +1,43 @@
+import { VFC } from 'react'
+import Link from 'next/link'
+import { useQuery } from '@apollo/client'
+import { GET_USERS } from '../queries/queries'
+import { GetUsersQuery } from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+const FetchMain: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    // 毎回最新のデータを取得。キャッシュに保持
+    // fetchPolicy: 'network-only',
+    // 毎回最新のデータを取得。キャッシュに保持。取得中はキャッシュを表示
+    fetchPolicy: 'cache-and-network',
+    // 初回は最新のデータを取得。次回からキャッシュのみ。
+    // fetchPolicy: 'cache-first',
+    // キャッシュを使わない。通常のAPIクライアントと同じ挙動
+    // fetchPolicy: 'no-cache',
+  })
+  if (error)
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">Hasura main page</p>
+      {console.log(data)}
+
+      {data?.users.map((user) => {
+        return (
+          <p className="my-1" key={user.id}>
+            {user.name}
+          </p>
+        )
+      })}
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  )
+}
+export default FetchMain
---
+++ pages/hasura-sub.tsx
@@ -0,0 +1,35 @@
+import { VFC } from 'react'
+import Link from 'next/link'
+import { useQuery } from '@apollo/client'
+import { GET_USERS_LOCAL } from '../queries/queries'
+import { GetUsersQuery } from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+const FetchSub: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS_LOCAL)
+  if (error)
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+  return (
+    <Layout title="Hsura fetchPolicy">
+      <p className="mb-6 font-bold">Direct read out from cache</p>
+
+      {data?.users.map((user) => {
+        console.log(data)
+        return (
+          <p className="my-1" key={user.id}>
+            {user.name}
+          </p>
+        )
+      })}
+      <Link href="/hasura-main">
+        <a className="mt-6">Back</a>
+      </Link>
+    </Layout>
+  )
+}
+
+export default FetchSub
